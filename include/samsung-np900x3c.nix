15b1966d47b4c3a075669bb2cca66ef3cdf54df4
David Heijkamp <david.heijkamp@naturalis.nl>
2015-12-30 16:53:31 +0100

    Change Gummiboot timeout to 1 sec for even faster boot times.

--- include/samsung-np900x3c.nix
+++ include/samsung-np900x3c.nix
@@ -16,6 +16,7 @@
 
   # Use the gummiboot efi boot loader.
   boot.loader.gummiboot.enable = true;
+  boot.loader.gummiboot.timeout = 1;
   boot.loader.efi.canTouchEfiVariables = true;
 
   hardware = {
